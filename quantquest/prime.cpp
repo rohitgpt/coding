b9ddd369650412297438fd105bf407b9ddc0e1e6
krullun <rohitgupta02051996@gmail.com>
2018-08-20 02:45:35 +0530

    Added files

---
+++ quantquest/prime.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+// #include <chrono>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+// using namespace std::chrono;
+
+// typedef std::chrono::high_resolution_clock::time_point timen;
+typedef long long ll;
+typedef long double ld;
+
+#define duration(a) std::chrono::duration_cast<std::chrono::nanoseconds>(a).count()
+#define timeNow() std::chrono::high_resolution_clock::now()
+#define f(i, n) for(int (i)=0; (i)<(n); (i)++)
+#define frp(i, a, b) for(int (i)=a; (i)<(b); (i)++)
+#define frn(i, a, b) for(int (i)=a; (i)>(b); (i)--)
+#define pb(a) push_back(a)
+
+#define p(aaa) cout<<"debug "<<aaa<<endl;
+#define PI 3.14159265358979323846
+
+const int mod=1e9+7;
+const int mx=1e3;
+
+
+bool isprime(int n)
+{
+  int i;
+  bool isPrime = true;
+
+  for(i = 2; i <= n / 2; ++i)
+  {
+      if(n % i == 0)
+      {
+          isPrime = false;
+          break;
+      }
+  }
+
+  return isPrime;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n=1000, count=0;
+	// static const int arr[] = 
+	vector<int> p{ 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997 };
+	cout<<p.size()<<endl;
+	// frp(i, 1, 	n+1) 
+	// 	if(isprime(i)) cout<<i<<", ";
+
+	f(i, p.size()){
+		frp(j, i+1, p.size()){
+			if(!isprime(p[i]+p[j]-1) && (p[i]+p[j])%2==1){
+				cout<<p[i]<<" "<<p[j]<<endl; 
+				count++;
+			}
+		}
+	}
+	cout<<count<<endl;
+
+	return 0;
+}
---
+++ quantquest/sol.cpp
@@ -0,0 +1,98 @@
+
+#include <stdio.h>
+
+/*
+
+BEGINNING OF PROBLEM STATEMENT:
+Mr. S. and Mr. P. are both perfect logicians, being able to correctly deduce
+any truth from any set of axioms.  Two integers (not necessarily unique) are
+somehow chosen such that each is within some specified range.  Mr. S.
+is given the sum of these two integers; Mr. P. is given the product of these
+two integers.  After receiving these numbers, the two logicians do not
+have any communication at all except the following dialogue:
+<<1>>   Mr. P.:  I do not know the two numbers.
+<<2>>   Mr. S.:  I knew that you didn't know the two numbers.
+<<3>>   Mr. P.:  Now I know the two numbers.
+<<4>>   Mr. S.:  Now I know the two numbers.
+
+Given that the above statements are absolutely truthful, what are the two
+numbers?
+
+END OF PROBLEM STATEMENT
+
+ */
+
+#define SMALLEST_MIN	1
+#define LARGEST_MIN	10
+#define SMALLEST_MAX	50
+#define LARGEST_MAX	500
+
+long P[(LARGEST_MAX + 1) * (LARGEST_MAX + 1)];		/* products */
+long S[(LARGEST_MAX + 1) + (LARGEST_MAX + 1)];		/*   sums   */
+
+find(long min, long max)
+{
+	long i, j;
+	/*
+	 *	count factorizations in P[]
+	 *	all P[n] > 1 satisfy <<1>>.
+	 */
+	for(i = 0; i <= max * max; ++i)
+		P[i] = 0;
+
+	for(i = min; i <= max; ++i)
+		for(j = i; j <= max; ++j)
+			++P[i * j];
+
+	/*
+	 *	decompose possible SUMs and check factorizations
+	 *		all S[n] == min - 1 satisfy <<2>>.
+	 */
+	for(i = min + min; i <= max + max; ++i) {
+
+		for(j = i / 2; j >= min; --j)
+			if(P[j * (i - j)] < 2)
+				break;
+
+		S[i] = j;
+	}
+
+	/*
+	 *	decompose SUMs which satisfy <<2>> and see which products
+	 *	they produce.  All (P[n] / 1000 == 1) satisfy <<3>>.
+	 */
+	for(i = min + min; i <= max + max; ++i)
+		if(S[i] == min - 1)
+			for(j = i / 2; j >= min; --j)
+				if(P[j * (i - j)] > 1)
+					P[j * (i - j)] += 1000;
+	/*
+	 *	decompose SUMs which satisfy <<2>> again and see which products
+	 *	satisfy <<3>>.  Any (S[n] == 999 + min) satisfies <<4>>
+	 */
+	for(i = min + min; i <= max + max; ++i)
+		if(S[i] == min - 1)
+			for(j = i / 2; j >= min; --j)
+				if(P[j * (i - j)] / 1000 == 1)
+					S[i] += 1000;
+	/*
+	 *	find the answer(s) and print them
+	 */
+	printf("[%d,%d]\n",min,max);
+	for(i = min + min; i <= max + max; ++i)
+		if(S[i] == 999 + min)
+			for(j = i / 2; j >= min; --j)
+				if(P[j * (i - j)] / 1000 == 1)
+					printf("{ %d %d }: S = %d, P = %d\n",
+						i - j, j, i, (i - j)  * j);
+}
+
+main()
+{
+	long min, max;
+
+	for (min = SMALLEST_MIN; min <= LARGEST_MIN; min ++)
+	    for (max = SMALLEST_MAX; max <= LARGEST_MAX; max++)
+		find(min,max);
+		return 0;
+}
