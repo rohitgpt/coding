56dd407c4703d1c87f6432cdbe812e880e633329
krullun <rohitgupta02051996@gmail.com>
2019-02-16 19:36:05 +0530

    Added files

--- codeforces/1097b.cpp
+++ codeforces/1097b.cpp
@@ -4,25 +4,23 @@
 #define mx 15;
 using namespace std;
 
-int n;
+int n, flag=0;
 vector<int> a;
-int flag=0;
 int dp(int i, int sum){
-	return dp(i+1, sum+a[i]) + dp(i+1, sum-a[i]);
-	if(i==n-1)
-		if((sum+a[i])%360==0 || (sum-a[i])%360==0) flag==0; 
-	if(flag==1){
-		return 1;
+	if(flag==1)	return 1;
+	if(i==n-1){
+		if((sum+a[i])%360==0 || (sum-a[i])%360==0) flag=1; 
+		return 0;
 	}
+	return dp(i+1, sum+a[i]) + dp(i+1, sum-a[i]);
 }
 
 int main(){
 	cin>>n;
 	a.resize(n);
 	f(i, n) cin>>a[i];
-	if(flag==1)
-		cout<<"YES"<<endl;
-	else
-		cout<<"NO"<<endl;
+	dp(0,0);
+	if(flag==1)	cout<<"YES"<<endl;
+	else	cout<<"NO"<<endl;
 	return 0;
 }
